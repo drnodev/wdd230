ff6239673b9558c9bdf5ba6bf4d17718fca85cf0
drno.dev <alexanortega@gmail.com>
2022-09-24 20:55:34 -0600

    last modified

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
 let year = new Date();
 
 document.querySelector("#year").innerHTML = year.getFullYear();
-document.getElementById("lu").innerHTML = new Date(document.lastModified);
+document.getElementById("lu").innerHTML = new Date(document.lastModified).toLocaleString();
