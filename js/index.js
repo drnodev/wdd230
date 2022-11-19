d77ca61b390c2287b03c24b729e3e6faee99d80b
drnodev <alexanortega@gmail.com>
2022-11-19 12:14:45 -0600

    Weather Test Api

--- js/index.js
+++
@@ -1,6 +0,0 @@
-let year = new Date();
-
-document.querySelector("#year").innerHTML = year.getFullYear();
-document.getElementById("lu").innerHTML = new Date(document.lastModified).toLocaleString();
-
-let xd = `Welcome back ${firstName}! You last visited on <strong>${lvdate}</strong>.`
